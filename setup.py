7a03329094b37c5a0aca07fc0e7a419f1b916a12
Pascal Sturmfels <psturm@cs.washington.edu>
2019-06-25 12:18:56 -0700

    some fixes for the pypi tools

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='attributionpriors',  
-     version='0.1',
-     scripts=['ops'] ,
-     author="Gabriel Erion, Joseph D. Janizek, and Pascal Sturmfels",
-     author_email="",
+     version='0.2.1',
+     author="The Lee Lab at UW",
+     author_email="psturm@cs.washington.edu",
      description="Tools for training explainable models using attribution priors.",
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     long_description_content_type="text/markdown",
      url="https://github.com/suinleelab/attributionpriors",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
-	"Intended Audience :: Science/Research",
-	"Topic :: Scientific/Engineering :: Artifical Intelligence",
+         "Intended Audience :: Science/Research",
+         "Topic :: Scientific/Engineering"
      ],
+    install_requires=['numpy', 'tensorflow'],  
  )
